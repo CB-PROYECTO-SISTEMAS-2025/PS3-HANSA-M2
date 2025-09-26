68e64e44cc93f1c64c12273e1acd18ef20c98ad5
daril <daril@DARIL>
2025-09-25 23:03:32 -0400

    Enhance logging in Login component and API utility; add console messages for better debugging during login and API configuration

--- PR2_Hansa-Frontend/src/utils/api.ts
+++ PR2_Hansa-Frontend/src/utils/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-console.log('API_BASE_URL:', API_BASE_URL); // Debug log
+console.log('🚀 API_BASE_URL:', API_BASE_URL); // Debug log
+console.log('🚀 Configuración de API cargada');
 
 const api = axios.create({
   baseURL: API_BASE_URL,
